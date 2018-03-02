eb2531b20b07e6f0f68f810a18af6fa4a0461d98
RHB <haleultimate@yahoo.com>
2018-03-01 22:21:11 -0800

    load_data to match ps1 data.env

--- load_data.R
+++ load_data.R
@@ -1,6 +1,8 @@
 #open hist files 
 load_data <- function(cybc.list) {
+  if (cybc.list[1] != "BTC") print("ERROR: Need BTC to be first in cybc.list to extract dates")
   wd <- getwd()
+  cname.list <- c(".O",".H",",L",".Close",".Volume",".Market.Cap",".Adjusted",".J",".R",".D",".V",".shout")
   for (cybcstr in cybc.list) {
     cmd_str <- paste0(cybcstr,".df <- read.csv('",wd,"/data/",cybcstr,"hist.csv',stringsAsFactors = FALSE)")
     print(cmd_str)
@@ -30,6 +32,45 @@ load_data <- function(cybc.list) {
     eval(parse(text=cmd_str))
     eval(parse(text=paste0("print(is.xts(data.env$",cybcstr,".xts))")))
     #eval(parse(text=paste0("rm(",cybcstr,".df,",cybcstr,".reverse)")))
+    # if (cybcstr == "BTC") {  #needs to be first in cybc.list
+    #   data.env$date.xts <- data.env$BTC.xts[,"Date"]
+    #   data.env$date_list <- date_list
+    # }
+    # cmd_str <- paste0("data.env$",cybcstr,".xts <- merge(data.env$date.xts,data.env$",cybcstr,".xts)")
+    # print(cmd_str)
+    # eval(parse(text=cmd_str))
+    cmd_str <- paste0("data.env$",cybcstr," <- data.env$",cybcstr,".xts[,c('Open','High','Low','Close','Volume','Market.Cap')]")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    cmd_str <- paste0("data.env$",cybcstr," <- gsub(',','',data.env$",cybcstr,")")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    cmd_str <- paste0("storage.mode(data.env$",cybcstr,") <- 'numeric'")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    cmd_str <- paste0("data.env$",cybcstr,"<- merge(data.env$",cybcstr,",data.env$",cybcstr,"[,'Close'])")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    cmd_str <- paste0("data.env$",cybcstr,"<- merge(data.env$",cybcstr,",(data.env$",cybcstr,"[,'High']*data.env$",cybcstr,"[,'Low'])^0.5)")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    cmd_str <- paste0("data.env$",cybcstr,"<- merge(data.env$",cybcstr,",sqrt(data.env$",cybcstr,"[,'High']*data.env$",cybcstr,"[,'Low']*data.env$",cybcstr,"[,'Close'])^(1/3))")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    cmd_str <- paste0("data.env$",cybcstr,"<- merge(data.env$",cybcstr,",(data.env$",cybcstr,"[,'Close']*data.env$",cybcstr,"[,'Volume']))")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    cmd_str <- paste0("data.env$",cybcstr,"<- merge(data.env$",cybcstr,",log(data.env$",cybcstr,"[,ncol(data.env$",cybcstr,")]))")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    cmd_str <- paste0("data.env$",cybcstr,"<- merge(data.env$",cybcstr,",(data.env$",cybcstr,"[,'Market.Cap']/data.env$",cybcstr,"[,'Close']))")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    print(paste0(cybcstr,cname.list))
+    
+    cmd_str <- paste0("colnames(data.env$",cybcstr,") <- paste0(cybcstr,cname.list)")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
   }
   #rm(a,cybc.length,cybcstr,date_list,i,wd)
   print(ls(data.env))
@@ -37,23 +78,35 @@ load_data <- function(cybc.list) {
 
 
 calc_look_forward <- function(cybc.str,lf=-1) {
-  ve.xts <- "var.env$Returns"
-  c.txt <- paste0("data.env$",cybc.str,".xts[,'Close']")
+  ve.xts <- paste0("var.env$",cybc.str)
+  c.txt <- paste0("as.numeric(gsub(',','',data.env$",cybc.str,".xts[,'Close']))")
   lagc.txt <- paste0("stats::lag(",c.txt,",lf)")
-  if (exists("Returns",where=var.env)) {
+  print(cybc.str)
+  eval(parse(text=paste0("print(",ve.xts,"[1,])")))
+  if (exists(cybc.str,where=var.env)) {
+    cmd_str <- paste0(ve.xts," <- cbind(",ve.xts,",",lagc.txt,")")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    cmd_str <- paste0(ve.xts," <- cbind(",ve.xts,",",c.txt,")")
+    print(cmd_str)
+    eval(parse(text=cmd_str))
+    
+    eval(parse(text=paste0("print(",ve.xts,"[1,])")))
     cmd_str <- ifelse(lf<0,
-                         paste0(ve.xts," <- cbind(",ve.xts,",log(as.numeric(",lagc.txt,")/as.numeric(",c.txt,")))"),
-                         paste0(ve.xts," <- cbind(",ve.xts,",log(as.numeric(",c.txt,")/as.numeric(",lagc.txt,")))"))
+                         paste0(ve.xts," <- cbind(",ve.xts,",log(",lagc.txt,"/",c.txt,"))"),
+                         paste0(ve.xts," <- cbind(",ve.xts,",log(",c.txt,"/",lagc.txt,"))"))
   } else {
     cmd_str <- ifelse(lf<0,
-                         paste0(ve.xts," <- log(as.numeric(",lagc.txt,")/as.numeric(",c.txt,"))"),
-                         paste0(ve.xts," <- log(as.numeric(",c.txt,")/as.numeric(",lagc.txt,"))"))
+                         paste0(ve.xts," <- as.xts(log(",lagc.txt,"/",c.txt,"),order.by=index(data.env$date.xts))"),
+                         paste0(ve.xts," <- as.xts(log(",c.txt,"/",lagc.txt,"),order.by=index(data.env$date.xts))"))
   }
   print(cmd_str)
   eval(parse(text=cmd_str))
+  eval(parse(text=paste0("print(",ve.xts,"[1,])")))
   cmd_str <- ifelse(lf<0,
                        paste0("colnames(",ve.xts,")[ncol(",ve.xts,")] <- '",cybc.str,"lfn",-lf,"'"),
                        paste0("colnames(",ve.xts,")[ncol(",ve.xts,")] <- '",cybc.str,"lf",lf,"'"))
   print(cmd_str)
   eval(parse(text=cmd_str))
+  eval(parse(text=paste0("print(",ve.xts,"[1,])")))
 }
--- main.R
+++ main.R
@@ -4,18 +4,32 @@ rm(list=ls(all=TRUE)[!ls(all=TRUE) %in% keep_list])
 library(quantmod)
 library(dplyr)
 source("load_data.R")
-cybc.list <- c("ETH","BTC","BCH","LTC","RPL")
+cybc.list <- c("BTC","ETH","BCH","LTC","RPL")  #"BTC" needs to be first
 
 if (!exists("data.env")) {
   data.env <<- new.env(parent=globalenv())
   load_data(cybc.list)
 }
 var.env <<- new.env(parent=globalenv())
-var.env$Returns <- as.numeric(data.env$BTC.xts[,"Close"])
-print(str(var.env$Returns))
-print(colnames(var.env$Returns))
-print(typeof(var.env$Returns))
+# for (cybc.str in cybc.list) {
+#   cmd_str <- paste0("var.env$",cybc.str," <- as.xts(as.numeric(gsub(',','',data.env$",cybc.str,".xts[,'Close']),
+#                     order.by=index(data.env$date.xts)))")
+#   
+#   eval(parse(text=cmd_str))
+# }
+#   print(cmd_str)
 
-for (cybc.str in c("BTC")) {
-  calc_look_forward(cybc.str)
-}
+# var.env$Returns <- data.env$BTC.xts
+# print(str(var.env$Returns))
+# print(colnames(var.env$Returns))
+# print(typeof(var.env$Returns))
+# print(is.xts(var.env$Returns))
+
+# for (cybc.str in c("BTC","LTC","RPL","ETH","BCH")) { #),"ETH","RPL")) {
+#   calc_look_forward(cybc.str)
+# }
+# 
+# rets <- cbind(var.env$BTC[,'BTClfn1'],var.env$ETH[,'ETHlfn1'],var.env$RPL[,'RPLlfn1'],var.env$LTC[,'LTClfn1'])
+# ret1 <- stats::lag(rets,1)
+# retslag <- cbind(rets,ret1)
+# print(cor(retslag,use="complete.obs"))
